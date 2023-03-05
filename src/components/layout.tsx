4147b5a421cad50b29cafb0eeeecda9a4fdd6268
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-05 23:04:58 +0100

    Added Global Style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,6 @@
+import { GlobalStyle } from "@/styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
 import Navbar from "./navbar";
 
 interface LayoutProps {
@@ -6,9 +9,10 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Navbar />
       {children}
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+    primary: '#006AFF',
+    secondary: '#DE4D86',
+    tertiary: '#23232D',
+    black: '#000000',
+    white: '#FFFFFF',
+    grey: '#C4C4C4',
+    font: {
+      size: {
+        xxxs: '.9rem',
+        xxs: '1.2rem',
+        xs: '1.8rem',
+        s: '2.4rem',
+        m: '3.5rem',
+        l: '5rem',
+        xl: '6.8rem',
+        xxl: '7.2rem',
+      },
+      family: {
+        montserrat: '"Montserrat", sans-serif',
+      },
+    },
+    mq: {
+      phone: '@media (max-width: 768px)',
+      tablet: '@media (min-width: 768px)',
+      desktop: '@media (min-width: 1024px)',
+      bigDesktop: '@media (min-width: 1536px)',
+      huge: '@media (min-width: 1536px)',
+    },
+  };
