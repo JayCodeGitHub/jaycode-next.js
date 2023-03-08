3c52a79cf1036662a16d1906970cdafe99675a9f
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-08 12:01:28 +0100

    Styled NavLink

--- src/components/NavLink/NavLink.styles.tsx
+++ src/components/NavLink/NavLink.styles.tsx
@@ -2,7 +2,27 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.white};
+  font-size: ${({ theme }) => theme.font.size.s};
+  text-decoration: none;
+  position: relative;
   &.active {
     color: ${({ theme }) => theme.primary};
+  }
+  &::before {
+    content: "";
+    width: 120%;
+    height: 4px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.primary};
+    position: absolute;
+    top: 110%;
+    left: -10%;
+    transform: scaleX(0);
+    transform-origin: 0 50%;
+    transition: transform 0.2s ease-in-out;
+  }
+  &:hover::before {
+    transform: scaleX(1);
   }
 `;
