7bbc7a77647b1103b1b003bc95efcd6ba4da072b
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-10 19:47:18 +0100

    Created files with styles for each page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,74 @@
+import { Wrapper } from '@/styles/pages/about.styles';
+
 export default function About() {
   return (
-    <>
-      <main>
-        <h1>About</h1>
-      </main>
-    </>
+    <Wrapper>
+      <h1>About</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
+        excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure soluta
+        fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam veritatis ipsam
+        dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate debitis laudantium eos
+        magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae quis corrupti soluta
+        assumenda!
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
+        excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure soluta
+        fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam veritatis ipsam
+        dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate debitis laudantium eos
+        magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae quis corrupti soluta
+        assumenda!
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
+        excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure soluta
+        fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam veritatis ipsam
+        dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate debitis laudantium eos
+        magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae quis corrupti soluta
+        assumenda!
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
+        excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure soluta
+        fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam veritatis ipsam
+        dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate debitis laudantium eos
+        magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae quis corrupti soluta
+        assumenda!
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
+        excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure soluta
+        fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam veritatis ipsam
+        dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate debitis laudantium eos
+        magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae quis corrupti soluta
+        assumenda!
+      </p>
+    </Wrapper>
   );
 }
