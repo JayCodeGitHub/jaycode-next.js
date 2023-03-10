a3ab2abe26a0f54285abdb7b6cfeacbd2007e21d
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-10 23:04:56 +0100

    Styled Header for Mobile

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -5,27 +5,24 @@ export interface HeaderProps {
 }
 
 export const Header = styled.header<HeaderProps>`
-    align-items: center;
-    justify-content: ${({secondary}) => (secondary ? 'center' : 'flex-start')};
     width: 100%;
-    height: ${({secondary}) => (secondary ? '20vh' : '60vh')};
+    height: ${({secondary}) => (secondary ? '35vh' : '70vh')};
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: ${({secondary}) => (secondary ? 'center' : 'flex-start')};
-    padding-left: ${({secondary}) => (secondary ? '0' : '15vw')};
     h1 {
         color: ${({ theme }) => theme.white};
-        font-size: ${({ theme }) => theme.font.size.xxl};
+        font-size: ${({ theme }) => theme.font.size.m};
         padding-left: 20px;
         display: flex;
         align-items: center;
         justify-content: ${({secondary}) => (secondary ? 'center' : 'flex-start')};
-        margin: 5rem 0;
+        margin: 2.5rem 0;
     }
     p {
-        color: ${({ theme }) => theme.white};
-        font-size: ${({ theme }) => theme.font.size.xl};
+      color: ${({ theme }) => theme.white};
+        font-size: ${({ theme }) => theme.font.size.s};
     }
     div {
         display: flex;
@@ -33,8 +30,29 @@ export const Header = styled.header<HeaderProps>`
         align-items: flex-start;
         flex-direction: row;
         padding-left: 2rem;
+        button {
+          margin-top: 2rem;
+        }
+    }
+    ${({ theme }) => theme.mq.desktop}  {
+        padding-left: ${({secondary}) => (secondary ? '0' : '15vw')};
+        width: auto;
+        height: ${({secondary}) => (secondary ? '20vh' : '60vh')};
+        h1 {
+            font-size: ${({ theme }) => theme.font.size.xxl};
+            margin: 5rem 0;
+        }
+        p {
+            font-size: ${({ theme }) => theme.font.size.xl};
+        }
+        div {
+            padding-right: 20%;
+            flex-direction: row;
+            align-items: center;
+            padding: 0;
             button {
-              margin-top: 2rem;
+                margin-top: 0;
             }
          }
+    }
 `
