8157ce5d1a02e81fa6902f7f54c508357b46b342
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-12 11:01:53 +0100

    Styled About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,50 @@
-import Header from '@/components/header/header';
 import { motion } from 'framer-motion';
-import { Wrapper } from '@/styles/pages/about.styles';
+import Image from 'next/image';
+import Header from '@/components/header/header';
+import {
+  Wrapper,
+  StyledSection,
+  ImageWrapper,
+  DescriptionWrapper,
+  StyledImage,
+  Title,
+  Paragraph,
+  StyledTechnologies,
+  Technologie,
+} from '@/styles/pages/about.styles';
 
 const MotionWrapper = motion(Wrapper);
 
 export default function About() {
+  const AboutPageItems = {
+    title: ['Hello!!! My name is Jakub', 'My Skills'],
+    paragraph: [
+      `
+      I like to create things that live on the internet. 
+      For several years I have been developing my skills in HTML, CSS, and JavaScript. 
+      I will be a good member of the team because I learn quickly and work well in a team.
+      `,
+      `
+      During my education I had the opportunity to create simple websites and large Full Stack applications. 
+      I also had the opportunity to work on commercial projects, where I learned how to work with clients and select technologies for specific requirements. 
+      I can create applications based on frameworks such as React and NEXT.JS. I have experience working with Git and GitHub.`,
+    ],
+    technologies: [
+      'React',
+      'JavaScript ES6+',
+      'HTML 5',
+      'CSS',
+      'Git',
+      'Tailwind CSS',
+      'TypeSctipt',
+      'Node.js',
+      'GraphQL',
+      'Webpack',
+      'Docker',
+    ],
+    image: '/images/profile.jpeg',
+    alt: 'Profile Image',
+  };
   return (
     <>
       <Header secondary title="about" />
@@ -14,72 +54,22 @@ export default function About() {
         transition={{ duration: 0.4, delay: 0.4, ease: 'easeInOut' }}
         exit={{ opacity: '0%' }}
       >
-        <h1>About</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
+        <StyledSection>
+          <DescriptionWrapper>
+            <Title>{AboutPageItems.title[0]}</Title>
+            <Paragraph>{AboutPageItems.paragraph[0]}</Paragraph>
+            <Paragraph>{AboutPageItems.paragraph[1]}</Paragraph>
+            <Title>{AboutPageItems.title[1]}</Title>
+            <StyledTechnologies>
+              {AboutPageItems.technologies.map((item, i) => (
+                <Technologie key={i}>{item}</Technologie>
+              ))}
+            </StyledTechnologies>
+          </DescriptionWrapper>
+          <ImageWrapper>
+            <Image src={AboutPageItems.image} alt={AboutPageItems.alt} width={500} height={450} />
+          </ImageWrapper>
+        </StyledSection>
       </MotionWrapper>
     </>
   );
--- src/styles/pages/about.styles.ts
+++ src/styles/pages/about.styles.ts
@@ -1,8 +1,75 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-     padding: 2.5vw;
+    padding: 2.5vw;
+    color: ${({ theme }) => theme.white};
     ${({ theme }) => theme.mq.bigDesktop} {
-        padding: 2.5vw 10vw;
+        padding: 2.5vw 15vw;
     }
+`
+
+export const StyledSection = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    ${({ theme }) => theme.mq.desktop} {
+        flex-direction: row;
+    }
+`
+
+export const ImageWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 50%;
+    }
+`
+
+export const StyledImage = styled.img`
+    width: 90%;
+    aspect-ratio: 1/1;
+    border-radius: 1.5rem;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 75%;
+    }
+`
+
+export const DescriptionWrapper = styled.div`
+    width: 100%;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 50%;
+    }
+`
+
+export const Title = styled.h2`
+    font-size: ${({ theme }) => theme.font.size.s};
+    ${({ theme }) => theme.mq.desktop} {
+        font-size: ${({ theme }) => theme.font.size.m};
+    }
+`
+
+export const Paragraph = styled.p`
+    font-size: ${({ theme }) => theme.font.size.xs};
+    color: ${({ theme }) => theme.grey};
+    width: 90%;
+    padding-left: 2rem;
+    ${({ theme }) => theme.mq.desktop} {
+        font-size: ${({ theme }) => theme.font.size.s};
+    }
+`
+
+export const StyledTechnologies = styled.ul`
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+`
+
+export const Technologie = styled.li`
+    padding: 0.5rem 1rem;
+    color: ${({ theme }) => theme.primary};
 `
