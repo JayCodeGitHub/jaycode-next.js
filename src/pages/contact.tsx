12a19216307c943b63269f00916384b62d59ba8c
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-12 02:07:21 +0100

    Styled Contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,13 @@
 import Header from '@/components/header/header';
 import { motion } from 'framer-motion';
-import { Wrapper } from '@/styles/pages/contact.styles';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { FiMail } from 'react-icons/fi';
+import {
+  Wrapper,
+  ContactWrapper,
+  ContactItem,
+  ContactItemsWrapper,
+} from '@/styles/pages/contact.styles';
 
 const MotionWrapper = motion(Wrapper);
 
@@ -14,72 +21,25 @@ export default function Contact() {
         transition={{ duration: 0.4, delay: 0.4, ease: 'easeInOut' }}
         exit={{ opacity: '0%' }}
       >
-        <h1>Contact</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio, id, incidunt
-          excepturi quibusdam aliquam eos, nobis nulla eaque sunt pariatur ad commodi hic iure
-          soluta fugiat deserunt? Repellendus, quo? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Sunt unde officia nam totam magnam. Cum saepe, deserunt velit quisquam
-          veritatis ipsam dolorem voluptatibus at laudantium maiores animi rem nobis aspernatur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, ab veritatis, cupiditate
-          debitis laudantium eos magnam, totam aperiam quis et officiis! Rem corporis in id! Beatae
-          quis corrupti soluta assumenda!
-        </p>
+        <ContactWrapper>
+          <ContactItemsWrapper>
+            <ContactItem
+              href="https://www.linkedin.com/in/jakub-gryzi%C5%84ski-b00296264/"
+              target="_blank"
+            >
+              <BsLinkedin />
+              LinkedIn
+            </ContactItem>
+            <ContactItem href="mailto:jakub.gryzinski@gmail.com">
+              <FiMail />
+              Email
+            </ContactItem>
+            <ContactItem href="https://github.com/JayCodeGitHub" target="_blank">
+              <BsGithub />
+              GitHub
+            </ContactItem>
+          </ContactItemsWrapper>
+        </ContactWrapper>
       </MotionWrapper>
     </>
   );
--- src/styles/pages/contact.styles.ts
+++ src/styles/pages/contact.styles.ts
@@ -1,8 +1,70 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-     padding: 2.5vw;
+    padding: 2.5vw;
+    color: ${({ theme }) => theme.white};
     ${({ theme }) => theme.mq.bigDesktop} {
-        padding: 2.5vw 10vw;
+        padding: 2.5vw 15vw;
+    }
+`
+
+export const ContactWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    ${({ theme }) => theme.mq.desktop} {  
+        height: 55vh;
+        flex-direction: row;
+    }
+`
+
+export const FormWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    ${({ theme }) => theme.mq.desktop} {        
+        width: 50%;
+    }
+`
+
+export const ContactItemsWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    margin: 2rem 0;
+    ${({ theme }) => theme.mq.desktop} {        
+        width: 100%;
+        margin: 0;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+`
+
+export const ContactItem = styled.a`
+    font-size: ${({ theme }) => theme.font.size.s};
+    text-decoration: none;
+    display: flex;
+    width: 90%;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 4rem 6rem;
+    border-radius: 2rem;
+    border: 2px solid ${({ theme }) => theme.theme};
+    margin: 2rem 0;
+    color: ${({ theme }) => theme.white};
+    transition: color 0.1s ease-in-out;
+    ${({ theme }) => theme.mq.desktop} {        
+        font-size: ${({ theme }) => theme.font.size.m};
+        width: auto;
+    }
+    &:hover {
+        color: ${({ theme }) => theme.grey};
+    }
+    svg {
+        margin: 0 2rem;
     }
 `
