ad1381e0cf508f021905eedd8519ae2390055849
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-12 11:49:09 +0100

    Moved ProjectsSliderItems to separate file

---
+++ src/assets/items/ProjectsSliderItems.tsx
@@ -0,0 +1,23 @@
+export const ProjectsSliderItems = [
+  {
+    name: 'Karate Team',
+    href: 'https://karate-team.netlify.app/',
+    github: 'https://github.com/JayCodeGitHub/karate-team.pl',
+    image: '/images/karate-team-mobile.png',
+    alt: 'Screenshot from Karate Team mobile website',
+  },
+  {
+    name: 'Champion Academy',
+    href: 'https://championacademy.pl/',
+    github: 'https://github.com/JayCodeGitHub/Final-Champion-Academy',
+    image: '/images/champion-academy-mobile.png',
+    alt: 'Screenshot from Champion Academy mobile website',
+  },
+  {
+    name: 'Happy Notes',
+    href: 'https://happy-notes.pl/',
+    github: 'https://github.com/JayCodeGitHub/Happy-Notes',
+    image: '/images/happy-notes-mobile.png',
+    alt: 'Screenshot from Happy Notes mobile website',
+  },
+];
