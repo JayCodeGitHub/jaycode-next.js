7e35eb9d9026ba0df0809296efa650fc58c3b3c6
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-13 15:43:07 +0100

    Added Simple Scroll animation to animationSection

--- src/components/animationSection/animationSection.tsx
+++ src/components/animationSection/animationSection.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import { useRef } from 'react';
 import { Wrapper, SvgWrapper } from './animationSection.styles';
 import Scene from '../../assets/icons/Scene.svg';
 import { AnimationSectionItems } from '@/assets/items/AnimationSectionItems';
+import { motion, Variants } from 'framer-motion';
+
+const MotionSvgWrapper = motion(SvgWrapper);
+const MotionWrapper = motion(Wrapper);
+
+const cardVariants: Variants = {
+  offscreen: {
+    opacity: 0,
+  },
+  onscreen: {
+    opacity: 1,
+  },
+};
 
 function AnimationSection() {
   return (
-    <Wrapper>
-      <SvgWrapper>
+    <MotionWrapper initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 1 }}>
+      <MotionSvgWrapper variants={cardVariants}>
         <Scene />
-      </SvgWrapper>
+      </MotionSvgWrapper>
       <div>
         <h1>{AnimationSectionItems.title}</h1>
         <p>{AnimationSectionItems.description}</p>
       </div>
-    </Wrapper>
+    </MotionWrapper>
   );
 }
 
