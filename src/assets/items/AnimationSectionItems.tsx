98db59d3a9b524995b285a3e823d23f10a716144
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-13 11:25:40 +0100

    Moved AnimationSection to separate file

---
+++ src/assets/items/AnimationSectionItems.tsx
@@ -0,0 +1,4 @@
+export const AnimationSectionItems = {
+  title: 'The applications I create are easy to use and easy to develop.',
+  description: `I have experience in writing applications in JavaScript ES6+ with use async and await. And experience with Git and GitHub`,
+};
--- src/components/animationSection/animationSection.tsx
+++ src/components/animationSection/animationSection.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Wrapper, SvgWrapper } from './animationSection.styles';
 import Scene from '../../assets/icons/Scene.svg';
+import { AnimationSectionItems } from '@/assets/items/AnimationSectionItems';
 
 function AnimationSection() {
-  const AnimationSectionItems = {
-    title: 'The applications I create are easy to use and easy to develop.',
-    description: `I have experience in writing applications in JavaScript ES6+ with use async and await. And experience with Git and GitHub`,
-  };
-
   return (
     <Wrapper>
       <SvgWrapper>
